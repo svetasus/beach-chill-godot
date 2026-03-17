d3ca4b3432dc2f1a92e9f3434b482db96c7c8c52
Svetlana Sutyrka <sutyrkas@gmail.com>
2026-03-17 22:34:00 +0100

    smaller shells

--- scripts/autospawner.gd
+++ scripts/autospawner.gd
@@ -108,6 +108,7 @@ func _spawn_item(item_scene: PackedScene, item_data: ItemData, pos: Vector3):
 		item_instance.data = item_data
 
 	_spawned_nodes.append(item_instance)
+	print("Spawning item ", item_instance.name)
 
 func _spawn_treasure(profile: TreasureProfile, pos: Vector3):
 	var treasure_instance = profile.treasure_scene.instantiate()
